6ba2cc6ea875acbc0436662bef5ab6379c9e7a0d
Julian Kennedy <juliankenn@gmail.com>
2016-07-13 15:59:49 +0200

    - no longer build a seperate memory file

--- build_glue_code.sh
+++ build_glue_code.sh
@@ -15,4 +15,12 @@ cd $root_dir
 # run build commands
 python $EMSCRIPTEN/tools/webidl_binder.py libnoise.idl glue
 
-em++ -O2 libnoise_glue.cpp -Lbuild/src -lnoise --post-js glue.js -s DEMANGLE_SUPPORT=1 -o build/libnoise.js
+em++ -O2 --memory-init-file 0 libnoise_glue.cpp -Lbuild/src -lnoise --post-js glue.js -s DEMANGLE_SUPPORT=1 -o build/libnoise.js
+
+# create npm package
+cd $PWD
+mkdir -p npmpackage
+cp package.json npmpackage/
+cp build/libnoise.js npmpackage/
+cp README.md npmpackage/
+cp LICENSE npmpackage/
