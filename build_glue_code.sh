644931d1257eae254ddc7e84745121f1f5fb4ce0
Julian Kennedy <juliankenn@gmail.com>
2017-07-03 16:00:24 +0200

    - fixed the module options for running with custom EXPORT_NAME

--- build_glue_code.sh
+++ build_glue_code.sh
@@ -15,9 +15,11 @@ cd $root_dir
 # run build commands
 python $EMSCRIPTEN/tools/webidl_binder.py libnoise.idl glue
 
-cat glue.js export_module.js > postfix.js
+#cat glue.js export_module.js > postfix.js
+cat glue.js > postfix.js
 
-em++ -O2 --memory-init-file 0 libnoise_glue.cpp -Lbuild/src -lnoise --post-js postfix.js -s DEMANGLE_SUPPORT=1 -o build/libnoise.js
+rm build/libnoise.js
+em++ -s EXPORT_NAME="'LibNoise'" -s MODULARIZE=1 -O2 --memory-init-file 0 libnoise_glue.cpp -Lbuild/src -lnoise --post-js postfix.js -s DEMANGLE_SUPPORT=1 -o build/libnoise.js
 
 # create npm package
 cd $PWD
