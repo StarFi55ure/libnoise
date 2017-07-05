54c3592bc498fa5fbb8620b88ad0dc2ee69629be
Julian Kennedy <juliankenn@gmail.com>
2017-07-05 09:30:43 +0200

    - added some debug assistance

--- build_glue_code.sh
+++ build_glue_code.sh
@@ -18,13 +18,14 @@ python $EMSCRIPTEN/tools/webidl_binder.py libnoise.idl glue
 #cat glue.js export_module.js > postfix.js
 cat glue.js > postfix.js
 
-rm build/libnoise.js
-em++ -s EXPORT_NAME="'LibNoise'" -s MODULARIZE=1 -O2 --memory-init-file 0 libnoise_glue.cpp -Lbuild/src -lnoise --post-js postfix.js -s DEMANGLE_SUPPORT=1 -o build/libnoise.js
+rm -f build/libnoise.js
+em++ -s EXPORT_NAME="'LibNoise'" -s MODULARIZE=1 -s ASSERTIONS=1 -O2 --memory-init-file 0 libnoise_glue.cpp -Lbuild/src -lnoise --post-js postfix.js -s DEMANGLE_SUPPORT=1 -o build/libnoise.js
 
 # create npm package
 cd $PWD
 mkdir -p npmpackage
 cp package.json npmpackage/
 cp build/libnoise.js npmpackage/
+#cp build/libnoise.js.mem npmpackage/
 cp README.md npmpackage/
 cp LICENSE npmpackage/
